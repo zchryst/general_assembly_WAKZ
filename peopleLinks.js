293f49250c2e96fa9853dacead8eeb72f6553c87
Zach Chrystall <z.chrystall@gmail.com>
2017-04-21 14:58:03 +1000

    very minor changes

--- peopleLinks.js
+++ peopleLinks.js
@@ -1,10 +1,3 @@
 console.log('hello world');
 
 var access_token = '35575e4e442a3e27402c7e342a78552e'
-
-$.ajax({
-  api: 'https://api.meetup.com/2/members?offset=0&format=json&group_urlname=Machine-Learning-AI-Meetup&photo-host=public&page=20&order=name&access_token=' + access_token,
-  method: 'get'
-}).done(function(results) {
-  console.log(results);
-});
