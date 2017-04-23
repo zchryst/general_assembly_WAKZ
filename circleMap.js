02c838c0f67c42241b140e461b709b8382321c14
William Miller <will.oliver.miller@gmail.com>
2017-04-23 10:11:02 +1000

    geocoder working, circles on map not working due to API reuests per/s

--- circleMap.js
+++ circleMap.js
@@ -11,9 +11,18 @@ function initialize() {
 
   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-  heatmap.setOptions({radius: heatmap.get(null)});
-
-  heatmap.setMap(map);
+  for (var country in countryArr) {
+    // Add the circle for this city to the map.
+    var countryCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: countryArr[country],
+      radius: Math.sqrt(countryList[country]) * 100
+    });
 
 }
 
@@ -26,26 +35,40 @@ function loadScript() {
 
 window.onload = loadScript;
 
-var surveyData = JSON.parse(survey);
+var countryList = { }
 
+_.map(survey, function (person) {
+  var userCountry = person.country
+    if (countryList[userCountry] != undefined) {
+      countryList[userCountry] = countryList[userCountry] + 1
+    }
+    else {
+      countryList[userCountry] = 1
+    }
+});
 
-var citymap = {
-  chicago: {
-    center: {lat: 41.878, lng: -87.629},
-    population: 2714856
+var countryOnly = _.keys(countryList)
+
+var countryArr = ['United Kingdom']
+
+var coordsLat = [];
+
+var coordsLon = [];
+
+function codeAddress(countryArr, callback) {
+  for(var i = 0; i < countryArr.length; i++) {
+    currCountry = countryArr[i];
+    var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({'address': countryArr[i]}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          coordsLat.push(results[0].geometry.location.lat())
+          coordsLon.push(results[0].geometry.location.lng())
+        } else {
+          throw('No results found: ' + status);
+      }
+    })
   }
-};
+}
 
-for (var country in surveyData) {
-    // Add the circle for this city to the map.
-    var countryCircle = new google.maps.Circle({
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
-      map: map,
-      center: citymap[city].center,
-      radius: Math.sqrt(citymap[city].population) * 100
-    });
-  }
+codeAddress(countryArr, function() {
+});
